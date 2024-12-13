851335b5441fe4392fff270c5d258fa475ff504f
Sermad Ahmad <sermadahmad024@gmail.com>
2024-12-13 14:53:10 +0500

    working on detail screen

--- App.js
+++ App.js
@@ -1,24 +1,89 @@
 import { View, Text } from "react-native";
 import React from "react";
-import HomeScreen from "./screens/homeScreen";
-import {MaterialCommunityIcons} from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { useState } from "react";
+import { CheckBox } from "react-native-btr";
 
 export default function App() {
+  const [isSelected, setSelection] = useState(true);
   return (
-      
-    <View>
-      <View>
-      <MaterialCommunityIcons name="volume-high" size={24} color="black" />
+    <View style={styles.main}>
+      {/* Icon Section */}
+      <View style={styles.iconBox}>
+        <MaterialCommunityIcons name="volume-high" size={24} color="gray" />
       </View>
-      <View>
-        <Text>title</Text>
-        <Text>jfkdh jdfhj jdfjdsh dfhj jfdhjshd jfdshj</Text>
-
+      {/* Details Section */}
+      <View style={styles.detailsBox}>
+        <Text style={styles.title}>Turn on sounds</Text>
+        <Text style={styles.desc}>Play sounds when changing values.</Text>
       </View>
-      <View>
-        
+      {/* Checkbox Section */}
+      <View style={styles.checkBox}>
+        <CheckBox
+          checked={isSelected}
+          onPress={() => setSelection(!isSelected)}
+          color="#4259A9"
+          disabled={false}
+          borderRadius={2}
+          borderWidth={2}
+        />
       </View>
-
     </View>
   );
 }
+
+const controls = [
+  {
+    title: "Turn on sounds",
+    desc: "Play sounds when changing values",
+  },
+  {
+    title: "Turn on vibration",
+    desc: "Vibrate on value changes",
+  },
+  {
+    title: "Use hardware buttons",
+    desc: "Change counter value using volume buttons",
+  },
+  {
+    title: "Tap value to increment",
+    desc: "Increment a counter by tapping the current value",
+  },
+];
+
+const display = {
+  title: "Keep the screen on",
+  desc: "Keep the screen on while the app is active",
+};
+
+const styles = {
+  main: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: 20,
+  },
+  iconBox: {
+    alignItems: "center",
+    justifyContent: "center",
+
+    paddingRight: 20,
+  },
+  detailsBox: {
+    flex: 1, // Allow this section to take available space
+    marginHorizontal: 10, // Add some spacing on the sides
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  desc: {
+    flexWrap: "wrap", // Ensure text wraps
+    fontSize: 14,
+  },
+  checkBox: {
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+  },
+};
